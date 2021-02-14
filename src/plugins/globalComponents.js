34797e123d57fde14a7d7758a702ae4d570f4ffe
a-ankitpatel <ankit22687@gmail.com>
2021-02-14 19:35:06 +1030

    remove unnecessary code for loader

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -2,9 +2,6 @@ import BaseButton from "../components/BaseButton";
 import BaseNav from "../components/BaseNav";
 import BaseTable from "../components/BaseTable";
 import BaseHeader from "../components/BaseHeader";
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
-import { CubeSpin } from 'vue-loading-spinner'
 
 export default {
   install(Vue) {
@@ -12,7 +9,5 @@ export default {
     Vue.component(BaseNav.name, BaseNav);
     Vue.component(BaseTable.name, BaseTable);
     Vue.component(BaseHeader.name, BaseHeader);
-    Vue.component("Loading", Loading);
-    Vue.component("Cubespin", CubeSpin);
   }
 };
